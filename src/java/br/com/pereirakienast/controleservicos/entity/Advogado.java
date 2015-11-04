1d205bd7e47728d2d96f95a613e88de67846b50e
paulopinheiro <paulopinheiro@pinheiro>
2015-11-03 22:51:46 -0200

    Implementação de geração de hash SHA para senha. tanto para persistência quanto para comparação

--- src/java/br/com/pereirakienast/controleservicos/entity/Advogado.java
+++ src/java/br/com/pereirakienast/controleservicos/entity/Advogado.java
@@ -1,9 +1,15 @@
 package br.com.pereirakienast.controleservicos.entity;
 
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.Collator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 /**
  *
@@ -13,8 +19,11 @@ import javax.persistence.Id;
 public class Advogado implements Comparable, Serializable {
     @Id
     private Integer id;
+    @NotNull
     private String oab;
-    private String md5Password;
+    @NotNull
+    private String digestPassword;
+    @NotNull
     private String nome;
     private boolean administrador;
     private boolean ativo;
@@ -35,12 +44,22 @@ public class Advogado implements Comparable, Serializable {
         this.oab = oab;
     }
 
-    public String getMd5Password() {
-        return md5Password;
+    /**
+     * 
+     * @return Hash da senha escolhida pelo usuário
+     */
+    public String getDigestPassword() {
+        return digestPassword;
     }
 
-    public void setMd5Password(String md5Password) {
-        this.md5Password = md5Password;
+    /**
+     * Insere hash da senha, gerado com algoritmo SHA para codificação
+     * UTF-8
+     * @param digestPassword a senha de onde se gerará o hash para persistir no
+     * banco de dados
+     */
+    public void setDigestPassword(String digestPassword) {
+        this.digestPassword = digestPassword(digestPassword);
     }
 
     public String getNome() {
@@ -65,6 +84,42 @@ public class Advogado implements Comparable, Serializable {
 
     public void setAtivo(boolean ativo) {
         this.ativo = ativo;
+    }
+
+    private static String digestPassword(String password) {
+        if ((password==null)||(password.isEmpty())) return null;
+        try {
+            byte[] bytesOfParameter;
+            bytesOfParameter = password.getBytes("UTF-8");
+            byte[] digest = MessageDigest.getInstance("SHA").digest(bytesOfParameter);
+            return new String(digest,"UTF-8");
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Advogado.class.getName()).log(Level.SEVERE, null, ex);
+            return password;
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(Advogado.class.getName()).log(Level.SEVERE, null, ex);
+            return password;
+        }
+    }
+
+    /**
+     * Gera hash da senha informada e compara com o hash anteriormente
+     * persistido. O hash da senha é implementada com algoritmo SHA para
+     * codificação UTF-8
+     * @param password a senha informada, de onde se gerará um hash para
+     * comparar com o persistido no banco de dados
+     * @return True se:
+     * 1. a senha informada é não nula;
+     * 2. o hash persistido for não nulo e
+     * 3. o hash da senha informada é igual ao persistido.
+     * False se quaisquer dessas condições falharem
+     */
+    public boolean confirmaPassword(String password) {
+        return (this.digestPassword != null) &&
+                (password !=null) &&
+                (!password.isEmpty()) &&
+               (!this.digestPassword.isEmpty()) &&
+               this.digestPassword.equals(digestPassword(password));
     }
 
     @Override
